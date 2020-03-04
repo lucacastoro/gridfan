a778cff8607ef37ff4db694203ad4bb3eb187f04
Luca Castoro <castoro.luc@gmail.com>
2020-03-04 02:20:45 +0000

    Add initial configurability through yaml-cpp and gflags

--- gridfan/main.cpp
+++ gridfan/main.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <unordered_map>
 #include <numeric>
+#include <memory>
+#include <variant>
 #include <csignal>
 #include <cmath>
 #include <ctime>
@@ -13,6 +15,9 @@
 #include "temperature.hpp"
 #include "libgridfan.hpp"
 #include "logger.hpp"
+
+#include "yaml-cpp/yaml.h"
+#include "gflags/gflags.h"
 
 using namespace std::chrono;
 using namespace std::chrono_literals;
@@ -58,6 +63,36 @@ static inline double linear( double temp ) {
   return clamp( min_spd, max_spd, int( ( temp - min_tmp ) * 100.0 / ( max_tmp - min_tmp ) ) );
 };
 
+class algo {
+public:
+  virtual double operator () (double) = 0;
+protected:
+  template <typename T>
+  T get(const YAML::Node& conf, const char* name, T def) {
+    if (conf[name]) return conf[name].as<T>();
+    return def;
+  }
+};
+
+class algo_linear : public algo {
+public:
+  algo_linear(const YAML::Node& conf) {
+    min_tmp = get(conf, "min_temp", 25.0);   // deg
+    max_tmp = get(conf, "max_temp", 70.0);   // deg
+    min_spd = get(conf, "min_speed", 10.0);  // %
+    max_spd = get(conf, "max_speed", 100.0); // %
+  }
+
+  virtual double operator () (double temp) override final {
+    return clamp( min_spd, max_spd, ( temp - min_tmp ) * 100.0 / ( max_tmp - min_tmp ) );
+  }
+private:
+  double min_tmp;
+  double max_tmp;
+  double min_spd;
+  double max_spd;
+};
+
 static inline double softplus( double x )
 {
   return log1p( exp( x ) );
@@ -68,42 +103,90 @@ static inline double logistic( double x )
   return 1.0 / ( 1.0 + exp( -x ) );
 }
 
-int main() {
+#define DEFAULT_CONFIG_FILE "/etc/gridfan/config.yml"
+#define DEFAULT_LOGGER "syslog"
+#define DEFAULT_ALGORITHM "linear"
 
-  signal( SIGINT, &sig_handler );
-  signal( SIGQUIT, &sig_handler );
-  signal( SIGTERM, &sig_handler );
-  signal( SIGUSR1, &sig_handler );
+DEFINE_string(config, DEFAULT_CONFIG_FILE, "configuration file");
+DEFINE_string(logger, DEFAULT_LOGGER, "logger");
+DEFINE_string(algorithm, DEFAULT_ALGORITHM, "algorithm");
 
-  using Log = SysLog;
+int main(int argc, char** argv) {
 
-  Log log;
+  signal(SIGINT, &sig_handler);
+  signal(SIGQUIT, &sig_handler);
+  signal(SIGTERM, &sig_handler);
+  signal(SIGUSR1, &sig_handler);
 
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  const std::string config_file = FLAGS_config.size() ? FLAGS_config : DEFAULT_CONFIG_FILE;
+
+  const auto load_config = [](const std::string& file) -> YAML::Node {
+    try {
+      return YAML::LoadFile(file);
+    } catch (std::exception& ex) {
+      return {};
+    }
+  };
+
+  const YAML::Node config = load_config(config_file);
+
+  if (not config.size() and FLAGS_config.size()) {
+    std::cerr << "cannot open " << FLAGS_config << std::endl;
+    return 1;
+  }
+
+  const auto get_conf = [&config](const char* name, const std::string& flag, const char* def) -> std::string {
+    if (flag.size()) return flag;
+    if (config[name]) return config[name].as<std::string>();
+    return def;
+  };
+
+  const auto log_type = get_conf("logger", FLAGS_logger, DEFAULT_LOGGER);
+  const auto algo_type = get_conf("algorithm", FLAGS_algorithm, DEFAULT_ALGORITHM);
+
+  std::unique_ptr<Logger> log;
+  if (log_type == "syslog") {
+    log = std::make_unique<SysLog>();
+  } else if (log_type == "output") {
+    log = std::make_unique<LocalLog>();
+  } else {
+    std::cerr << "invalid logger value" << std::endl;
+    return 1;
+  }
+
+  const std::unique_ptr<algo> algorithm = algo_type == "linear"
+    ? std::make_unique<algo_linear>(config)
+    : nullptr;
+  
+  if (not algorithm) {
+     log->error("invalid algorithm: %s", algo_type.c_str());
+     return 1;
+  }
+  
   grid::controller controller(std::nothrow);
 
-  if( not controller ) {
-    log.error("cannot access the fan controller");
+  if (not controller) {
+    log->error("cannot access the fan controller");
     return 1;
   }
 
   temperature::monitor monitor;
 
-  if( not monitor ) {
-    log.error("cannot access the temperature monitor");
+  if (not monitor) {
+    log->error("cannot access the temperature monitor");
     return 1;
   }
 
   const auto cpu = monitor.find("CPU Temperature");
 
-  if( cpu == monitor.end()) {
-    log.error("cannot find the CPU temperature sensor");
+  if (cpu == monitor.end()) {
+    log->error("cannot find the CPU temperature sensor");
     return 1;
   }
 
-  log.info("started");
-
-  using func_t = double (*) (double);
-  func_t func = &linear;
+  log->info("applying %s algorithm", algo_type.c_str());
 
   static constexpr milliseconds interval = 1s;
   int last_p = -1;
@@ -111,20 +194,19 @@ int main() {
   size_t errors = 0;
   constexpr size_t max_errors = 5;
 
-  while( not stop ) {
+  while (not stop) {
 
-    try
-    {
+    try {
       const auto t = cpu->temperature();
-      const auto p = int( func( t ) );
+      const auto p = int( (*algorithm)( t ) );
 
       if (verbose_trigger) {
         verbose_trigger = false;
         verbose = not verbose;
-        log.info("verbose mode %s", verbose ? "activated" : "deactivated");
+        log->info("verbose mode %s", verbose ? "activated" : "deactivated");
         if (verbose) {
-          log.info("current temperature is %.2f degree", t);
-          log.info("current speed is %d%%", p);
+          log->info("current temperature is %.2f degree", t);
+          log->info("current speed is %d%%", p);
         }
       }
 
@@ -143,7 +225,7 @@ int main() {
         }
 
         if (verbose) {
-          log.info("setting fans speed to %d%%", last_p);
+          log->info("cpu is %.2f degree, setting fans speed to %d%%", t, last_p);
         }
 
         for (auto& fan : controller) {
@@ -155,34 +237,31 @@ int main() {
       interruptible_sleep(interval);
       if (stop) break;
     }
-    catch( const std::exception& ex )
-    {
-      if( ++errors == max_errors )
-      {
-        log.error("exception caught: %s", ex.what());
-        log.error("too many errors, giving up");
+    catch( const std::exception& ex ) {
+      if( ++errors == max_errors ) {
+        log->error("exception caught: %s", ex.what());
+        log->error("too many errors, giving up");
         stop = true;
       }
-      else
-      {
-        log.warning("exception caught: %s", ex.what());
+      else {
+        log->warning("exception caught: %s", ex.what());
 
         interruptible_sleep(5s);
         if (stop) break;
 
         controller = grid::controller();
 
-        if( not controller )
-        {
-          log.error("could not re-initialize the controller");
+        if( not controller ) {
+          log->error("could not re-initialize the controller");
           stop = true;
         }
       }
     }
   }
 
-  if (got_signal)
-    log.info("got signal '%s' (%d)", strsignal(got_signal), got_signal);
+  if (got_signal) {
+    log->info("got signal '%s' (%d)", strsignal(got_signal), got_signal);
+  }
 
-  log.info("terminated");
+  log->info("terminated");
 }
